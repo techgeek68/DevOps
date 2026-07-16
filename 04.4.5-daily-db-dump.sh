77527a1db236e302850295512adc2564ec5cb05a
techgeek68 <pyakurelelx@gmail.com>
2026-07-16 12:47:12 +0545

    Remove old content and keep only Syllabus and Module 1 to Module 5

--- 04.4.5-daily-db-dump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Dumps MySQL database for daily backup
-DB="production_db"
-USER="root"
-PASS="yourpassword"
-BACKUP_DIR="/opt/db_backups"
-mkdir -p $BACKUP_DIR
-mysqldump -u $USER -p$PASS $DB > $BACKUP_DIR/${DB}_$(date +%F).sql
